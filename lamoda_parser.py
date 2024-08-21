da878dcd642327fba879f1182ded34eb58aef517
Aleksandr Sazhin <alexss2610@gmail.com>
2024-08-21 21:52:14 +0500

    Изменил стандартное ожидание

--- lamoda_parser.py
+++ lamoda_parser.py
@@ -6,7 +6,7 @@ arg_parser = ArgumentParser()
 
 
 if __name__ == "__main__":
-    arg_parser.add_argument("-md", "--mdelay", type=float, default=1, help="Main routine delay")
+    arg_parser.add_argument("-md", "--mdelay", type=float, default=0.5, help="Main routine delay")
     args = arg_parser.parse_args()
     lamoda_parser = LamodaParser(mdelay=args.mdelay)
     lamoda_parser.start()
