6107ebf286f1875a970b14a2933bbdbed541221b
Sazhin Aleksandr <alexss2610@gmail.com>
2025-02-19 17:45:24 +0500

    Некоторые изменения в db_clear.py для проверки

--- db_clear.py
+++ db_clear.py
@@ -11,16 +11,16 @@ if __name__ == '__main__':
     #arg_parser.add_argument('-d', '--dir', type=str, help='Путь к файлу sqlite')
     #args = arg_parser.parse_args()
     #engine = create_engine(f"sqlite:///{args.dir}")
-    engine = create_engine(f"sqlite:///./output2/db.sqlite3")
+    engine = create_engine(f"sqlite:///./output/output_1734098285331/db.sqlite3")
     dup_count = 0
     with Session(engine) as session:
-        all_items = session.query(LamodaItem).all()
+        '''all_items = session.query(LamodaItem).all()
         for item in all_items:
             item.img_rel_path = str(item.img_rel_path).replace(os.sep, '/')
-        session.commit()
-        print(f'Всего объектов в базе {len(all_items)}')
+        session.commit()'''
+        #print(f'Всего объектов в базе {len(all_items)}')
         #dups = session.query(LamodaItem).having(func.count(LamodaItem.img_rel_path) > 1).group_by(LamodaItem.img_rel_path).all()
-        '''subq = (
+        subq = (
             session.query(LamodaItem.img_rel_path, func.min(LamodaItem.id).label("min_id")).group_by(LamodaItem.img_rel_path)
             ).subquery('pth_min_id')
         q_duplicates = (session.query(LamodaItem).join(subq, and_(
@@ -28,10 +28,15 @@ if __name__ == '__main__':
             ))
 
         for item in q_duplicates:
+            #session.delete(item)
+            dup_count += 1
+            print(item.img_rel_path)
+
+        for item in q_duplicates:
             session.delete(item)
-            dup_count += 0
+            dup_count += 1
         session.commit()
 
-        print(f'Было удалено {dup_count} записей')'''
+        print(f'Было удалено {dup_count} записей')
         
 
